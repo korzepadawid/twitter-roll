1c9c288a75c02328214523a2237c272c8f1054e0
korzepadawid <korzepadawid@yahoo.com>
2022-09-24 11:13:43 +0200

    feat: init

---
+++ README.md
@@ -0,0 +1,1 @@
+# twitter-barca-roll
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"go.uber.org/zap"
+)
+
+func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logger.Info("Started.")
+}
